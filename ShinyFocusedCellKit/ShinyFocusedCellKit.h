3ee2df88109e1aada09e9e8386074162a550ce41
Yung Dai <yungchidai@gmail.com>
2019-09-26 07:59:25 -0400

    Initial Commit

---
+++ ShinyFocusedCellKit/ShinyFocusedCellKit.h
@@ -0,0 +1,19 @@
+//
+//  ShinyFocusedCellKit.h
+//  ShinyFocusedCellKit
+//
+//  Created by Yung Dai on 2019-09-26.
+//  Copyright © 2019 None. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShinyFocusedCellKit.
+FOUNDATION_EXPORT double ShinyFocusedCellKitVersionNumber;
+
+//! Project version string for ShinyFocusedCellKit.
+FOUNDATION_EXPORT const unsigned char ShinyFocusedCellKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShinyFocusedCellKit/PublicHeader.h>
+
+
